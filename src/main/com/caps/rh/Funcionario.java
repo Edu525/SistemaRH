1b3e7ec21ce320c7e7ff077f39af6bd09ca66730
Edu525 <edu88100@gmail.com>
2026-04-08 20:10:37 +0000

    ajustes na index

---
+++ src/main/com/caps/rh/Funcionario.java
@@ -0,0 +1,20 @@
+package.com.caps.rh ;
+
+public class Funcionario {
+    private String nome;
+    private double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+}
