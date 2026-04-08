45e6a00361ebeadd740d83a4b3f46faf188f93ce
Edu525 <edu88100@gmail.com>
2026-04-08 20:23:05 +0000

    Funcionario.Java

--- src/main/com/caps/rh/Funcionario.java
+++ src/main/com/caps/rh/Funcionario.java
@@ -1,20 +1,42 @@
-package.com.caps.rh ;
+package com.caps.rh;
 
 public class Funcionario {
     private String nome;
     private double salario;
+    private String cargo;
 
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        this.cargo = cargo;
     }
 
     public String getNome() {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public double getSalario() {
         return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void aplicar Aumento(double percentual) {
+        this.salario += this.salario * (percentual / 100);
     }
 
 }
